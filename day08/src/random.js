ed24d38b9290ac36a06b0473adab40121f408852
whrudwls08 <whrudwls08@softbowl.co.kr>
2025-01-21 13:55:54 +0900

    first commit

---
+++ day08/src/random.js
@@ -0,0 +1,12 @@
+import chance from "chance";
+const change = new chance();
+
+/* chance를 이용해 무작위 이름을 생성해 반환 */
+export function getRandomName() {
+  return change.name();
+}
+
+/* chance를 이용해 무작위 이메일을 생성해 반환 */
+export function getRandomEmail() {
+  return change.email();
+}
